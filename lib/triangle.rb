84dce2285d5c8569f1fd587063b1f6d6f07959d1
Irene <irenemanningm@gmail.com>
2022-08-14 17:24:49 -0500

    commit tree 1346ee53114b02a4b9123a67fc5b07dabcfb8176

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    kind
+  end 
+  def kind
+    validate_triangle
+    if [a, b, c].uniq.length == 1
+      :equilateral
+    elsif [self.a, self.b, self.c].uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def validate_triangle
+     
+     if [self.a, self.b, self.c].any? { |side| side == 0 }
+      raise TriangleError
+     end
+    if a + b <= c || b + c <= a || a + c <= b
+      raise TriangleError
+    end
+  end
+   class TriangleError < StandardError
+   end
 end
